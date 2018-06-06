1b5d570780e0131d48bc6591db3b228ef593b833
Seddst <39424218+Seddst@users.noreply.github.com>
2018-06-05 21:54:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def hello(bot, update):
         'Hello {}'.format(update.message.from_user.first_name))
 
 
-updater = Updater('YOUR TOKEN HERE')
+updater = Updater('602671114:AAFO7AajY4QtufjBgYG7y8dQb4IcpeLgWss')
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
