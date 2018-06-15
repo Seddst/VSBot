e26078b1497922df08bde9eb35636f7d31c2a99e
Seddst <39424218+Seddst@users.noreply.github.com>
2018-06-14 19:28:36 -0700

    Create template.py

---
+++ template.py
@@ -0,0 +1,13 @@
+from VSBot.core.types import User
+
+
+def fill_template(msg: str, user: User):
+    if user.username:
+        msg = msg.replace('%username%', '@' + user.username)
+    else:
+        msg = msg.replace('%username%', (user.first_name or '') + ' ' + (user.last_name or ''))
+    msg = msg.replace('%first_name%', user.first_name or '')
+    msg = msg.replace('%last_name%', user.last_name or '')
+    msg = msg.replace('%id%', str(user.id))
+    msg = msg.replace('%ign%', (user.first_name or '') + ' ' + (user.last_name or ''))
+    return msg
