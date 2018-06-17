ecbebbd507b588f9374c955379431bd8701be224
Seddst <39424218+Seddst@users.noreply.github.com>
2018-06-16 22:32:05 -0700

    Create pin.py

---
+++ core/functions/pin.py
@@ -0,0 +1,57 @@
+from telegram import Bot, Update
+
+from VSBot.core.texts import MSG_PIN_ALL_ENABLED, MSG_PIN_ALL_DISABLED
+from VSBot.core.types import Admin, admin_allowed, user_allowed, check_admin
+from VSBot.core.utils import update_group, send_async
+
+# adminTypes need a new name
+
+
+def pin_decorator(func):
+    @user_allowed
+    def wrapper(bot, update, session, *args, **kwargs):
+        group = update_group(update.message.chat, session)
+        if group is None and \
+                check_admin(update, session, Admin.FULL) or \
+                group is not None and \
+                (group.allow_pin_all or
+                 check_admin(update, session, Admin.GROUP)):
+            func(bot, update, session, *args, **kwargs)
+
+    return wrapper
+
+
+@pin_decorator
+def pin(bot: Bot, update: Update):
+    bot.request.post(bot.base_url + '/pinChatMessage',
+                     {'chat_id': update.message.reply_to_message.chat.id,
+                      'message_id': update.message.reply_to_message.message_id,
+                      'disable_notification': False})
+
+
+@pin_decorator
+def silent_pin(bot: Bot, update: Update):
+    bot.request.post(bot.base_url + '/pinChatMessage',
+                     {'chat_id': update.message.reply_to_message.chat.id,
+                      'message_id': update.message.reply_to_message.message_id,
+                      'disable_notification': True})
+
+
+@admin_allowed(Admin.GROUP)
+def pin_all(bot: Bot, update: Update, session):
+    group = update_group(update.message.chat, session)
+    if not group.allow_pin_all:
+        group.allow_pin_all = True
+        session.add(group)
+        session.commit()
+    send_async(bot, chat_id=update.message.chat.id, text=MSG_PIN_ALL_ENABLED)
+
+
+@admin_allowed(Admin.GROUP)
+def not_pin_all(bot: Bot, update: Update, session):
+    group = update_group(update.message.chat, session)
+    if group.allow_pin_all:
+        group.allow_pin_all = False
+        session.add(group)
+        session.commit()
+    send_async(bot, chat_id=update.message.chat.id, text=MSG_PIN_ALL_DISABLED)
