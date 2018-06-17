455923da5365127270f7526f678ad6ae37a26ad1
Seddst <39424218+Seddst@users.noreply.github.com>
2018-06-16 22:43:53 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,16 @@
 # finish this later
+TOKEN = '<618668131:AAG57FawPhzS5FEBsyGRsSZ1aIxbh871Ei0>'  # token telegram (is obtained from @BotFather)
+
+# string (only supported by MySql, you can read about the format here
+# http://docs.sqlalchemy.org/en/latest/dialects/mysql.html)
+API_PORT = 3306  # Port, on which external api is available (REST)
+GOVERNMENT_CHAT = 591505188  # Chat in which the bot will sent reports (current number is set to vs main chat id
+#  but isn't being used in main)
+SUPER_ADMIN_ID = 0  # id супер админа
+# functional recruiting:
+Venture_CHAT_ID = None  # id chateau chat (to find new players in the castle - if None - functional is disabled)
+ACADEM_CHAT_ID = None  # id chat academy (for notification of new players in the castle- if None -
+# functional is disabled)
+CRYSTALBALL = None  # flag of the castle, which you want to serve (for example '🇮🇲'),
+# leave None, so there are no restrictions
+WEB_LINK = 'Please change it. {}'
