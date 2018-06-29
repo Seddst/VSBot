61d56905b0830ad173edce48ee60aef0ba519dbb
Seddst <39424218+Seddst@users.noreply.github.com>
2018-06-29 09:34:56 -0700

    Update newbies.py

--- core/functions/newbies.py
+++ core/functions/newbies.py
@@ -10,12 +10,12 @@ from config import ACADEM_CHAT_ID, Venture_CHAT_ID
 
 @user_allowed
 def newbie(bot: Bot, update: Update, session):
-    if ACADEM_CHAT_ID and Venture_CHAT_ID:
+    if Venture_CHAT_ID:
         if update.message.chat.id in [Venture_CHAT_ID]:
             for new_chat_member in update.message.new_chat_members:
                 user = session.query(User).filter(User.id == new_chat_member.id).first()
                 if user is None:
-                    group = session.query(Group).filter(Group.id == ACADEM_CHAT_ID).first()
+                    group = session.query(Group).filter(Group.id == Venture_CHAT_ID).first()
                     if group is not None:
                         send_async(bot, chat_id=group.id,
                                    text=fill_template(MSG_NEWBIE, new_chat_member))
