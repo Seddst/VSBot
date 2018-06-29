e2aae986a588f1a2b7d6c43a7cbbbeedccdee7cf
Seddst <39424218+Seddst@users.noreply.github.com>
2018-06-29 10:39:47 -0700

    Update newbies.py

--- core/functions/newbies.py
+++ core/functions/newbies.py
@@ -3,19 +3,19 @@ from core.types import User, Group, user_allowed
 from core.template import fill_template
 from core.utils import send_async
 from core.texts import *
-from config import Venture_CHAT_ID
+from config import VENTURE_CHAT_ID
 
 # fix academ chat we're not using it
 
 
 @user_allowed
 def newbie(bot: Bot, update: Update, session):
-    if Venture_CHAT_ID:
-        if update.message.chat.id in [Venture_CHAT_ID]:
+    if VENTURE_CHAT_ID:
+        if update.message.chat.id in [VENTURE_CHAT_ID]:
             for new_chat_member in update.message.new_chat_members:
                 user = session.query(User).filter(User.id == new_chat_member.id).first()
                 if user is None:
-                    group = session.query(Group).filter(Group.id == Venture_CHAT_ID).first()
+                    group = session.query(Group).filter(Group.id == VENTURE_CHAT_ID).first()
                     if group is not None:
                         send_async(bot, chat_id=group.id,
                                    text=fill_template(MSG_NEWBIE, new_chat_member))
