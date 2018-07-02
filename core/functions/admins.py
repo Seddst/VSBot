58e5422880770bbfb5519a3b1dbb7002326686cb
Seddst <39424218+Seddst@users.noreply.github.com>
2018-07-02 16:32:45 -0700

    Update admins.py

--- core/functions/admins.py
+++ core/functions/admins.py
@@ -6,9 +6,13 @@ from core.texts import (
     MSG_NEW_GROUP_ADMIN_EXISTS, MSG_DEL_GROUP_ADMIN_NOT_EXIST,
     MSG_LIST_ADMINS_HEADER, MSG_LIST_ADMINS_FORMAT,
     MSG_EMPTY, MSG_LIST_ADMINS_USER_FORMAT,
+    MSG_NEW_GLOBAL_ADMIN, MSG_NEW_GLOBAL_ADMIN_EXISTS,
+    MSG_NEW_SUPER_ADMIN, MSG_NEW_SUPER_ADMIN_EXISTS,
+    MSG_DEL_GLOBAL_ADMIN, MSG_DEL_GLOBAL_ADMIN_NOT_EXIST
     )
 from core.types import User, AdminType, Admin, admin_allowed, user_allowed
 from core.utils import send_async
+from config import SUPER_ADMIN_ID
 
 
 @admin_allowed()
@@ -120,3 +124,123 @@ def admins_for_users(bot: Bot, update: Update, session):
                                                       user.last_name or '')
 
     send_async(bot, chat_id=update.message.chat.id, text=msg)
+
+
+@admin_allowed(adm_type=AdminType.SUPER)
+def set_global_admin(bot: Bot, update: Update, session):
+    msg = update.message.text.split(' ', 1)[1]
+    msg = msg.replace('@', '')
+    if msg != '':
+        user = session.query(User).filter_by(username=msg).first()
+        if user is None:
+            send_async(bot,
+                       chat_id=update.message.chat.id,
+                       text=MSG_USER_UNKNOWN)
+
+        else:
+            adm = session.query(Admin).filter_by(user_id=user.id,
+                                                 admin_type=AdminType.FULL.value).first()
+
+            if adm is None:
+                new_group_admin = Admin(user_id=user.id,
+                                        admin_type=AdminType.FULL.value,
+                                        admin_group=0)
+                session.add(new_group_admin)
+                session.commit()
+                send_async(bot,
+                           chat_id=update.message.chat.id,
+                           text=MSG_NEW_GLOBAL_ADMIN.format(user.username))
+
+            else:
+                send_async(bot,
+                           chat_id=update.message.chat.id,
+                           text=MSG_NEW_GLOBAL_ADMIN_EXISTS.format(user.username))
+
+
+@user_allowed(False)
+def set_super_admin(bot: Bot, update: Update, session):
+    msg = update.message.text.split(' ', 1)[1]
+    msg = msg.replace('@', '')
+    if msg != '':
+        user = session.query(User).filter_by(username=msg).first()
+        if user is None:
+            send_async(bot,
+                       chat_id=update.message.chat.id,
+                       text=MSG_USER_UNKNOWN)
+
+        else:
+            if user.id == SUPER_ADMIN_ID and update.message.from_user.id == SUPER_ADMIN_ID:
+                adm = session.query(Admin).filter_by(user_id=user.id, admin_group=0).first()
+                if adm is not None:
+                    if adm.admin_type == AdminType.SUPER.value:
+                        send_async(bot,
+                                   chat_id=update.message.chat.id,
+                                   text=MSG_NEW_SUPER_ADMIN_EXISTS.format(user.username))
+
+                    else:
+                        adm.admin_type = AdminType.SUPER.value
+                        session.add(adm)
+                        session.commit()
+                        send_async(bot,
+                                   chat_id=update.message.chat.id,
+                                   text=MSG_NEW_SUPER_ADMIN.format(user.username))
+
+                else:
+                    new_super_admin = Admin(user_id=user.id,
+                                            admin_type=AdminType.SUPER.value,
+                                            admin_group=0)
+
+                    session.add(new_super_admin)
+                    session.commit()
+                    send_async(bot,
+                               chat_id=update.message.chat.id,
+                               text=MSG_NEW_SUPER_ADMIN.format(user.username))
+
+
+@admin_allowed(adm_type=AdminType.SUPER)
+def del_global_admin(bot: Bot, update: Update, session):
+    msg = update.message.text.split(' ', 1)[1]
+    if msg.find('@') != -1:
+        msg = msg.replace('@', '')
+        if msg != '':
+            user = session.query(User).filter_by(username=msg).first()
+            if user is None:
+                send_async(bot,
+                           chat_id=update.message.chat.id,
+                           text=MSG_USER_UNKNOWN)
+
+            else:
+                adm = session.query(Admin).filter_by(user_id=user.id,
+                                                     admin_type=AdminType.FULL.value).first()
+
+                if adm is None:
+                    send_async(bot,
+                               chat_id=update.message.chat.id,
+                               text=MSG_DEL_GLOBAL_ADMIN_NOT_EXIST.format(user.username))
+
+                else:
+                    session.delete(adm)
+                    session.commit()
+                    send_async(bot,
+                               chat_id=update.message.chat.id,
+                               text=MSG_DEL_GLOBAL_ADMIN.format(user.username))
+    else:
+        user = session.query(User).filter_by(id=msg).first()
+        if user is None:
+            send_async(bot,
+                       chat_id=update.message.chat.id,
+                       text=MSG_USER_UNKNOWN)
+
+        else:
+            adm = session.query(Admin).filter_by(user_id=user.id,
+                                                 admin_type=AdminType.FULL.value).first()
+            if adm is None:
+                send_async(bot,
+                           chat_id=update.message.chat.id,
+                           text=MSG_DEL_GLOBAL_ADMIN_NOT_EXIST.format(user.username))
+            else:
+                session.delete(adm)
+                session.commit()
+                send_async(bot,
+                           chat_id=update.message.chat.id,
+                           text=MSG_DEL_GLOBAL_ADMIN.format(user.username))
